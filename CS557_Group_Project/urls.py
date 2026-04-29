1644b9ed649642738a089e00b41af395ddeb64ef
BrennanArf <arfstro3@uwm.edu>
2026-04-29 16:49:26 -0500

    added login html page as home page

--- CS557_Group_Project/urls.py
+++ CS557_Group_Project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
