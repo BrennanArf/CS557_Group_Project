1963a49c91f6b4ceff3c2b5dd89c5f8c91a83ebc
BrennanArf <arfstro3@uwm.edu>
2026-04-30 13:19:35 -0500

    login additions, with redirect to home page. Need signup and user models

--- UWM_Track_and_Field_Performance_Management_System/views.py
+++ UWM_Track_and_Field_Performance_Management_System/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.views import View
 
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect('login')
