8e69cd0d4f3bc480f45e51ca4791b7a21a46247c
BrennanArf <arfstro3@uwm.edu>
2026-05-12 11:38:00 -0500

    sited models for admin use

--- UWM_Track_and_Field_Performance_Management_System/admin.py
+++ UWM_Track_and_Field_Performance_Management_System/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, Athlete, CompetitionResult, FallTesting, PracticeResult
 
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(Athlete)
+admin.site.register(CompetitionResult)
+admin.site.register(FallTesting)
+admin.site.register(PracticeResult)
