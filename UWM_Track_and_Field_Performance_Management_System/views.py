9607ae1ad4318d6c3adc6fcbeab850c3f3b6a432
Austin Witte <ajwitte@uwm.edu>
2026-05-12 13:11:53 -0500

    imports fixed

--- UWM_Track_and_Field_Performance_Management_System/views.py
+++ UWM_Track_and_Field_Performance_Management_System/views.py
@@ -18,7 +18,6 @@ class HomeView(LoginRequiredMixin,View):
 
 class AthleteView(LoginRequiredMixin,View):
     def get(self, request):
-        editing_id = -1
         search_query = request.GET.get('search', '')
         if request.user.role == "Student":
             athletes = Athlete.objects.filter(athlete_id=request.user.athlete_id)
@@ -33,7 +32,7 @@ class AthleteView(LoginRequiredMixin,View):
             
         context = {
             'athletes': athletes, 
-            'editing_id':int(editing_id),
+            'editing_id':int(editing_id) if editing_id else None,
             'search_query': search_query}
         return render(request, 'athletes.html', context)
     
@@ -119,7 +118,6 @@ class FallTestingView(LoginRequiredMixin, View):
 class PracticeResultView(LoginRequiredMixin, View):
     def get(self, request):
         search_query = request.GET.get('search', '')
-        editing_id = -1
         if request.user.role == "Student":
             practice_data = PracticeResult.objects.filter(athlete_id=request.user.athlete_id)
         else:
@@ -133,7 +131,7 @@ class PracticeResultView(LoginRequiredMixin, View):
 
         context = {
             'practice_results': practice_data,
-            'editing_id':int(editing_id),
+            'editing_id':int(editing_id) if editing_id else None,
             'search_query': search_query
         }
         return render(request, 'practice.html', context)
@@ -158,7 +156,7 @@ class PracticeResultView(LoginRequiredMixin, View):
                 approach = request.POST.get('approach', ''),
                 gear = request.POST.get('gear', ''),
                 mark_meters = to_float_if_not_null(request.POST.get('mark_meters', '')),
-                mark_raw = request.POST.get('mark_raw', ''),
+                mark_raw = to_float_if_not_null(request.POST.get('mark_raw', '')),
                 season = request.POST.get('season', '')
             )
             practice_result.save()
